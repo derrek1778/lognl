3b388224c2fbdc0d819a7549bb420b6682547241
derrek1789@gmail.com <derrek1789@gmail.com>
2020-02-22 21:47:09 -0500

    changes to index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 //import express module
 const express = require('express');
-
+//import mongoose
+const mongoose = require('mongoose');
+//add keys config
+const keys = require('./config/keys');
 //create new instance of Express
 const app = express();
+
+//open connection to mongo
+mongoose.connect(keys.mongoURI);
 
 //default route handler
 app.get('/', (req, resp) => { 
     resp.send({ Hi: 'Hello World' });
 });
 
+const PORT = process.env.PORT || 4000;
 //Open port
-app.listen(4000);
+app.listen(PORT);
