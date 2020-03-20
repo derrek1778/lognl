3d5910afc1e1e381bf8fe189331260d14b68f809
vasudevr <vasudev@vasudevs-mac.home>
2020-03-19 21:27:04 -0400

    updating client side with redux and materialize

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,10 +1,27 @@
+//import materialized css from node_module
+import 'materialize-css/dist/css/materialize.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+
+//create redux store
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+        <Provider
+            store={store}>
+            <App />
+        </Provider>
+        , document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
